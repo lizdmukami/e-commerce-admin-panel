bdedee184f52adb0116bad8d073d64ae6983df42
lizdmukami <lizdmukami@gmail.com>
2023-02-03 17:05:58 +0300

    Add buttons on Cards 1. install react-icons 2. Add buttons on product card 3. Change image height

--- src/Products/ProductCard.js
+++ src/Products/ProductCard.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { BsPencil } from 'react-icons/bs';
+import { RiDeleteBinLine } from 'react-icons/ri';
 
-function ProductCard({title, category, price, image}) {
+function ProductCard({ title, category, price, image }) {
   return (
     <div className="col-sm-4 card">
       <img className="card-img-top" src={image} alt="Card image cap" />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
-        <p className="card-text">
-          {category}
-        </p>
-        <p className="card-text">
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </p>
+        <p className="card-text text-danger">{category}</p>
+        <h3 className="text-dark">${price}</h3>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+          <button type="button" className="btn btn-outline-success px-5">
+            <BsPencil />
+          </button>
+          <button type="button" className="btn btn-outline-danger px-5">
+            <RiDeleteBinLine />
+          </button>
+        </div>
       </div>
     </div>
   );
